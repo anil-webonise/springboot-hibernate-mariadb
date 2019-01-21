adfa1421335525916c2edff4fbfc1f455539bb67
anil ingle <anil.ingale@weboniselab.com>
2019-01-21 14:34:57 +0530

    project dir. added

---
+++ springboot-hibernate-mariadb-crud/src/main/java/com/krushidj/springboothibernatemariadbcrud/SpringbootHibernateMariadbCrudApplication.java
@@ -0,0 +1,14 @@
+package com.krushidj.springboothibernatemariadbcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateMariadbCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateMariadbCrudApplication.class, args);
+	}
+
+}
+
