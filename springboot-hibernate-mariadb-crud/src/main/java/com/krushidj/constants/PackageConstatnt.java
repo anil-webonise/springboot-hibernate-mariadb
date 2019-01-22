ba1a57fa190f6b28bfdc84f0c1ebbecd0c783ba8
anil ingle <anil.ingale@weboniselab.com>
2019-01-22 16:32:35 +0530

    constant file added

---
+++ springboot-hibernate-mariadb-crud/src/main/java/com/krushidj/constants/PackageConstatnt.java
@@ -0,0 +1,5 @@
+package com.krushidj.constants;
+
+public class PackageConstatnt {
+    public  static final String adminControllerBasePackage="com.krushidj.modules.admin.controller";
+}
---
+++ springboot-hibernate-mariadb-crud/src/main/java/com/krushidj/constants/SwaggerMetaDataInfoConstant.java
@@ -0,0 +1,9 @@
+package com.krushidj.constants;
+
+public class SwaggerMetaDataInfoConstant {
+
+    public static final String swaggerTitle = "krushidj admin  API";
+    public static final String swaggerDesciption = "krushidj admin api in spring boot..!";
+    public static final String swaggerProfile = "!production";
+
+}
